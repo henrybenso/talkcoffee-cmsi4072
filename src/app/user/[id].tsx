e802d147abbf184a661946dcfb5090d169d26223
henrybenso <henrykbenso1@gmail.com>
2023-07-30 12:12:14 -0700

    worked on user id page

--- src/app/user/[id].tsx
+++ src/app/user/[id].tsx
@@ -3,5 +3,15 @@ export default function Profile({
 }: {
   userData: {
     id: string;
+    email: string;
+    username: string;
+    hashedPassword: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    role: string;
+    birthDate: string;
+    createdAt: string;
+    updatedAt: string;
   };
 }) {}
