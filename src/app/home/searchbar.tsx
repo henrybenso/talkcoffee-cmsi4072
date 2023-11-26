34d9de1275a551c90c41268a077b6c8d99055ea0
edwardphillip <112529094+edwardphillip@users.noreply.github.com>
2023-11-26 15:17:50 -0800

    Searchbar Modified

--- src/app/home/searchbar.tsx
+++ src/app/home/searchbar.tsx
@@ -1,38 +1,56 @@
 "use client";
 
-// import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import React, { useState } from 'react';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const { replace } = useRouter();
-    if (term) {
-      params.set("query", term);
-    } else {
-      params.delete("query");
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`/api/search?term=${searchTerm}`);
+      const result = await response.json();
+
+      console.log('Search result:', result);
+    } catch (error) {
+      console.error('Error searching:', error);
     }
-    replace("${pathname}?${params.toString()}");
-    console.log(term);
-  }
+
+    const params = new URLSearchParams(searchParams);
+    if (searchTerm) {
+      params.set('query', searchTerm);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("query")?.toString()}
-      />
-      {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+          placeholder="Type your search here"
+          onChange={handleSearchChange}
+          value={searchTerm}
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
