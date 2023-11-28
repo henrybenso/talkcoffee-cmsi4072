8e12e8aa29218ace19891e5e7fd4509fa62c13ae
henrybenso <henrykbenso1@gmail.com>
2023-11-28 13:03:20 -0800

    adding upload folders

---
+++ src/app/stores/[id]/page.tsx
@@ -0,0 +1,27 @@
+// import Head from "next/head";
+// import Layout from "../../layout";
+// import Headerinfo from "../headerinfo";
+// import ImageScroller from "../imageScroller";
+// import { buttonVariants } from "@/components/ui/button";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// export default function Page({ store }) {
+//   return (
+//     <Layout>
+//       <>
+//         <Image></Image>
+//         <div>
+//           <Link
+//             href="/stores/create"
+//             className={buttonVariants({ variant: "outline" })}
+//           >
+//             Add a store
+//           </Link>
+//         </div>
+//         <div>Google Rating:</div>
+//         <div>Talk Coffee Rating:</div>
+//       </>
+//     </Layout>
+//   );
+// }
